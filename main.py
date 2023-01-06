f8d126a4561385c546ca1004f4c74d0da0296723
Mikko <mijuraut@gmail.com>
2023-01-06 15:53:52 +0200

    beautiful soup start

--- main.py
+++ main.py
@@ -9,5 +9,23 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(soup.title)
 # print(soup.title.string)
 
-print(soup.prettify())
-print(soup.p)
+# print(soup.prettify())
+# print(soup.p)
+
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    # print(tag.getText())
+    print(tag.get("href"))
+
+#heading = soup.find_all("h3")
+#print(heading)
+
+# h3_heading = soup.find_all(name="h3", class_="heading")
+# print(h3_heading)
+
+#name = soup.select_one(selector="#")
+#print(name)
+
+#headings = soup.select(".heading")
+#print(headings)
